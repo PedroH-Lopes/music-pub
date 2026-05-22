70a11a5a2b235dd8288ec64e6dff7bb684729235
pedrolopes <pedroh.lopes86@gmail.com>
2026-05-22 13:29:34 -0300

    feat: first commit

---
+++ src/main/java/com/pedrohlopes/musicPub/MusicPubApplication.java
@@ -0,0 +1,13 @@
+package com.pedrohlopes.musicPub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrohlopes/musicPub/MusicPubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrohlopes.musicPub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
