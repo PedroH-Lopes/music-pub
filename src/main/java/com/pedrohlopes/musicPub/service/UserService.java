b28a0201fd72b301efe9e11510068215d6538e7a
pedrolopes <pedroh.lopes86@gmail.com>
2026-05-26 20:56:04 -0300

    feat: created validateEmail method to check if a email is already registered

--- src/main/java/com/pedrohlopes/musicPub/service/UserService.java
+++ src/main/java/com/pedrohlopes/musicPub/service/UserService.java
@@ -1,5 +1,6 @@
 package com.pedrohlopes.musicPub.service;
 
+import com.pedrohlopes.musicPub.exception.BusinessException;
 import com.pedrohlopes.musicPub.model.user.UserEntity;
 import com.pedrohlopes.musicPub.repository.IUserRepository;
 import com.pedrohlopes.musicPub.model.user.Roles;
@@ -14,11 +15,19 @@ public class UserService {
 
     public UserEntity registerUser(String name, String email, String password, Roles role) {
 
+        validateEmail(email);
+
         return userRepository.save(UserEntity.builder()
                     .name(name)
                     .email(email)
                     .password(password)
                     .role(role)
                 .build());
+    }
+
+    public void validateEmail(String email) {
+        if (userRepository.existsByEmail(email)) {
+            throw new BusinessException("Email já cadastrado");
+        }
     }
 }
