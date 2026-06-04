2f554ee85dae04f322e331b3af93c55873850b32
pedrolopes <pedroh.lopes86@gmail.com>
2026-06-04 14:07:22 -0300

    feat: added spring security dependency with jwt token generation logic

---
+++ src/main/java/com/pedrohlopes/musicPub/service/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.pedrohlopes.musicPub.service;
+
+import com.pedrohlopes.musicPub.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final IUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+    }
+}
